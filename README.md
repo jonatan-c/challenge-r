255c853295fc99e382adb1bc7929e785187e9bd2
jonatan-c <jona.cc.93@gmail.com>
2024-06-11 19:57:21 -0300

    feat: finished challenge

--- README.md
+++ README.md
@@ -1,36 +1,28 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+ ### Challenge Superliga Recursiva
 
-## Getting Started
+## Description
 
-First, run the development server:
+This is a project to allow upload a file .csv and show the data in a page.
+
+# Technologies
+
+- Next
+- Tailwind
+- Typescript
+
+## Installation
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+git clone <repo>
+
+node version 20.11.0
+
+npm install
+
+npm run dev 
+
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Comments
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+In this case i did not use backend for optimize the resources and not loading it in the browser, but in a real case i will use a backend to upload the file and save in a database. Make the request to the backend and show the data in the page with a better performance.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,266 @@
+"use client";
+import React, { useState } from "react";
+import Papa from "papaparse";
 import Image from "next/image";
 
-export default function Home() {
+type Socio = {
+  id: number;
+  name: string;
+  age: number;
+  team: string;
+  condition: string;
+  education: string;
+};
+
+const Home = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [partners, setPartners] = useState<Socio[]>([]);
+  const [results, setResults] = useState<any | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const processAndCalculate = () => {
+    if (!file) return;
+
+    Papa.parse(file, {
+      header: false,
+      delimiter: ";",
+      encoding: "ISO-8859-1",
+      complete: (result) => {
+        let partners = result.data.map((s: any, index: number) => ({
+          id: index + 1,
+          name: s[0],
+          age: parseInt(s[1], 10),
+          team: s[2],
+          condition: s[3],
+          education: s[4],
+        }));
+
+        partners.pop();
+
+        setPartners(partners);
+
+        const totalPartners = partners.length;
+
+        const racingPartners = partners.filter((s) => s.team === "Racing");
+        const averageAgeRacing =
+          racingPartners.reduce((acc, s) => acc + s.age, 0) /
+          racingPartners.length;
+
+        const universityMarried = partners
+          .filter(
+            (s) => s.condition === "Casado" && s.education === "Universitario"
+          )
+          .sort((a, b) => a.age - b.age)
+          .slice(0, 100)
+          .map((s) => ({ name: s.name, age: s.age, team: s.team }));
+
+        const fansRiver = partners.filter((s) => s.team === "River");
+        const nombresMasComunesRiver = Object.entries(
+          fansRiver.reduce((acc, s) => {
+            acc[s.name] = (acc[s.name] || 0) + 1;
+            return acc;
+          }, {} as Record<string, number>)
+        )
+          .sort(([, a], [, b]) => b - a)
+          .slice(0, 5)
+          .map(([name]) => name);
+
+        const teams = Array.from(new Set(partners.map((s) => s.team)));
+        const teamsStats = teams
+          .map((team) => {
+            const partnersTeam = partners.filter((s) => s.team === team);
+            const totalAge = partnersTeam.reduce((acc, s) => acc + s.age, 0);
+            const averageAge = totalAge / partnersTeam.length;
+            const younger = Math.min(...partnersTeam.map((s) => s.age));
+            const older = Math.max(...partnersTeam.map((s) => s.age));
+
+            return {
+              team,
+              averageAge,
+              younger,
+              older,
+              quantityPartners: partnersTeam.length,
+            };
+          })
+          .sort((a, b) => b.quantityPartners - a.quantityPartners);
+
+        setResults({
+          totalPartners,
+          averageAgeRacing,
+          universityMarried,
+          nombresMasComunesRiver,
+          teamsStats,
+        });
+      },
+    });
+  };
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const currentItems = results
+    ? results.universityMarried.slice(
+        (currentPage - 1) * itemsPerPage,
+        currentPage * itemsPerPage
+      )
+    : [];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="flex flex-col lg:flex-row p-6 justify-center items-center lg:h-screen h-full">
+        <div className="max-w-md mx-auto p-6 bg-white text-black rounded-lg shadow-md space-y-4 h-fit  lg:w-[50%] ">
+          <div className="space-y-4 text-center">
+            <label
+              htmlFor="fileInput"
+              className="flex flex-row items-cente justify-center gap-6 cursor-pointer"
+            >
+              <Image src="/mas.png" alt="Logo" width={50} height={50} />
+              <div className="flex flex-col">
+                <span>Añade tus archivos</span>
+                <span className="underline">o seleccione una carpeta</span>
+              </div>
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              accept=".csv"
+              onChange={handleFileChange}
+              className="hidden"
             />
-          </a>
+
+            <button
+              onClick={processAndCalculate}
+              disabled={!file}
+              className="w-52 bg-blue-500   hover:bg-blue-700 text-white font-bold py-2 mt-12 px-4 rounded-3xl disabled:opacity-50"
+            >
+              Enviar
+            </button>
+          </div>
+        </div>
+
+        <div className="space-y-4 mt-4 lg:w-[50%] w-full justify-normal items-start h-full">
+          {results && (
+            <div className="space-y-4 mt-4">
+              <h3 className="font-semibold">
+                Total de personas registradas: {results.totalPartners}
+              </h3>
+              <h3 className="font-semibold">
+                Promedio de edad de los partners de Racing:{" "}
+                {results.averageAgeRacing.toFixed(2)}
+              </h3>
+
+              <div>
+                <h3 className="font-semibold mb-4">
+                  Primeras 100 personas casadas con estudios universitarios
+                </h3>
+                <ul className="pl-5 text-center list-none">
+                  {currentItems.map((s: Socio, index: number) => (
+                    <li key={index}>
+                      {s.name}, {s.age} años, {s.team}
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex flex-row justify-center mt-4">
+                  Página {currentPage} de{" "}
+                  {results
+                    ? Math.ceil(results.universityMarried.length / itemsPerPage)
+                    : 1}
+                </div>
+                <div className="flex flex-row justify-center mt-4">
+                  <button
+                    onClick={() => setCurrentPage(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    className="mr-2 border border-gray-300 px-2 py-1 rounded-lg"
+                  >
+                    Anterior
+                  </button>
+                  <button
+                    onClick={() => setCurrentPage(currentPage + 1)}
+                    disabled={
+                      results
+                        ? currentPage ===
+                          Math.ceil(
+                            results.universityMarried.length / itemsPerPage
+                          )
+                        : true
+                    }
+                    className="mr-2 border border-gray-300 px-2 py-1 rounded-lg"
+                  >
+                    Siguiente
+                  </button>
+                </div>
+              </div>
+
+              <div>
+                <h3 className="font-semibold">
+                  5 nombres más comunes entre los hinchas de River
+                </h3>
+                <ul className="list-none pl-5">
+                  {results.nombresMasComunesRiver.map(
+                    (name: string, index: string) => (
+                      <li key={index}>{name}</li>
+                    )
+                  )}
+                </ul>
+              </div>
+
+              <div className="mb-10 pb-10">
+                <h3 className="font-semibold">Estadísticas por team</h3>
+                <div className="overflow-x-auto">
+                  <table className="min-w-full divide-y divide-gray-200 table-fixed text-white">
+                    <thead className="bg-slate-400 divide-y divide-gray-200 dark:bg-slate-700 mb-6 text-white  ">
+                      <tr>
+                        <th className="px-3 py-2 text-left text-xs font-medium   uppercase tracking-wider">
+                          Equipo
+                        </th>
+                        <th className="px-3 py-2 text-left text-xs font-medium   uppercase tracking-wider">
+                          Cantidad Socios
+                        </th>
+                        <th className="px-3 py-2 text-left text-xs font-medium   uppercase tracking-wider">
+                          Edad Promedio
+                        </th>
+                        <th className="px-3 py-2 text-left text-xs font-medium   uppercase tracking-wider">
+                          Menor Edad
+                        </th>
+                        <th className="px-3 py-2 text-left text-xs font-medium   uppercase tracking-wider">
+                          Mayor Edad
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-slate-400 divide-y divide-gray-200 dark:bg-slate-700 mb-6">
+                      {results.teamsStats.map((e: any, index: string) => (
+                        <tr key={index}>
+                          <td className="px-3 py-2 whitespace-nowrap">
+                            {e.team}
+                          </td>
+                          <td className="px-3 py-2 whitespace-nowrap">
+                            {e.quantityPartners}
+                          </td>
+                          <td className="px-3 py-2 whitespace-nowrap">
+                            {e.averageAge.toFixed(2)}
+                          </td>
+                          <td className="px-3 py-2 whitespace-nowrap">
+                            {e.younger}
+                          </td>
+                          <td className="px-3 py-2 whitespace-nowrap">
+                            {e.older}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
-}
+};
+
+export default Home;
